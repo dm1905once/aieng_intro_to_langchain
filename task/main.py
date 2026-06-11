e779be5a288c920028bd139f8288f391c8385917
danielm <dm1905once@gmail.com>
2026-06-10 22:47:11 -0700

    Testing different binding

--- task/main.py
+++ task/main.py
@@ -107,7 +107,9 @@ tool_dictionary = {
     "PlanetRevolutionPeriod": planet_revolution_period_tool,
     "PlanetGeneralInfo": planet_general_info_tool
 }
-llm_with_tools = llm.bind_tools(list(tool_dictionary.values()))
+#llm_with_tools = llm.bind_tools(list(tool_dictionary.values()))
+llm_with_tools = llm.bind(tools=list(tool_dictionary.values()))
+
 
 @chain
 def function_call(llm_output) ->str:
